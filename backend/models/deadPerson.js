fa1fcde02bdf05994ae32dd049cea7e6d383a9cd
vsmeyukha <vsmeyukha@gmail.com>
2021-11-18 17:05:54 +0300

    написали модель умершего

---
+++ backend/models/deadPerson.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validation = require('validator');
+const errors = require('../constants/errors');
+
+const deadPersonSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  surname: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  patronymic: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+  },
+  dateOfBirth: {
+    type: Date,
+    min: '01-01-1900',
+    max: Date(),
+  },
+  dateOfDeath: {
+    type: Date,
+    max: Date(),
+  },
+  placeOfBirth: {
+    type: String,
+  },
+  placeOfDeath: {
+    type: String,
+  },
+  description: {
+    type: String,
+    minlength: 2,
+    maxlength: 2000,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('deadPerson', deadPersonSchema);
